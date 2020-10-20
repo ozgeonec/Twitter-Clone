1df5df6e52fe01e9587cc4f3b31287cce12f093b
ozgeonec <ozgenuronec@gmail.com>
2020-10-20 21:12:38 +0300

    icons filled version settings

--- components/navigation.js
+++ components/navigation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './navigation.module.css'
-import cn from 'classnames'
+
 import {
   Twitter,
   Home,
@@ -10,7 +10,14 @@ import {
   Bookmark,
   Lists,
   Profile,
-  More
+  More,
+  HomeFill,
+  ExplorerFill,
+  NotificationFill,
+  MessagesFill,
+  BookmarkFill,
+  ListsFill,
+  ProfileFill
 } from './icons'
 import NavButton from './nav-button'
 import TextTitle from './text-title'
@@ -19,71 +26,77 @@ const MENU = [
   {
     key: 'twitter',
     icon: <Twitter />,
+    iconSelected: <Twitter />,
     title: '',
     notify: 0
   },
   {
     key: 'home',
     icon: <Home />,
+    iconSelected: <HomeFill />,
     title: 'Home',
     notify: 0
   },
   {
     key: 'explore',
     icon: <Explore />,
+    iconSelected: <ExplorerFill />,
     title: 'Explore',
     notify: 0
   },
   {
     key: 'notifications',
     icon: <Notification />,
+    iconSelected: <NotificationFill />,
     title: 'Notifications',
     notify: 17
   },
   {
     key: 'messages',
     icon: <Messages />,
+    iconSelected: <MessagesFill />,
     title: 'Messages',
     notify: 0
   },
   {
     key: 'bookmarks',
     icon: <Bookmark />,
+    iconSelected: <BookmarkFill />,
     title: 'Bookmarks',
     notify: 0
   },
   {
     key: 'lists',
     icon: <Lists />,
+    iconSelected: <ListsFill />,
     title: 'Lists',
     notify: 0
   },
   {
     key: 'profile',
     icon: <Profile />,
+    iconSelected: <ProfileFill />,
     title: 'Profile',
     notify: 0
   },
   {
     key: 'more',
     icon: <More />,
+    iconSelected: <More />,
     title: 'More',
     notify: 0
   }
 ]
 
-function Navigation({ flat = false, selectedKey }) {
+function Navigation({ flat = false, selectedKey = 'home' }) {
   return (
     <nav className={styles.nav}>
       {MENU.map((menu) => {
-        const showTitle = !flat || menu.title.length === 0
+        const showTitle = !flat && menu.title.length > 0
+        const selected = selectedKey === menu.key
         return (
-          <NavButton
-            key={menu.key}
-            notify={menu.notify}
-            selected={selectedKey === menu.key}
-          >
-            {menu.icon}
+          <NavButton key={menu.key} notify={menu.notify} selected={selected}>
+            {selected ? menu.iconSelected : menu.icon}
             {showTitle && <TextTitle>{menu.title}</TextTitle>}
           </NavButton>
         )
